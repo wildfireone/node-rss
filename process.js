080d3d4e6db28f7892fbc907e48f4c3b997d2e74
wildfireone <wildfireone@gmail.com>
2016-01-20 22:39:15 +0000

    Update process.js

--- process.js
+++ process.js
@@ -1,1 +1,17 @@
 //
+var feed = require("feed-read");
+feed("https://api.flickr.com/services/feeds/photos_public.gne?id=55336091@N08&lang=en-us&format=rss_200", function(err, articles) {
+  if (err) throw err;
+  articles.forEach(function(article) {
+    console.log(article.title);
+  });
+  // Each article has the following properties:
+  // 
+  //   * "title"     - The article title (String).
+  //   * "author"    - The author's name (String).
+  //   * "link"      - The original article link (String).
+  //   * "content"   - The HTML content of the article (String).
+  //   * "published" - The date that the article was published (Date).
+  //   * "feed"      - {name, source, link}
+  // 
+});
