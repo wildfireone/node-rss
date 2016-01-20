777f0a6ce69d036fbf930b1c29e2f8a4514ccbcf
wildfireone <wildfireone@gmail.com>
2016-01-20 23:03:10 +0000

    Update process.js

--- process.js
+++ process.js
@@ -1,17 +1,18 @@
 //
-var feed = require("feed-read");
-feed("https://500px.com/johnisaacs/rss", function(err, articles) {
-  if (err) throw err;
-  articles.forEach(function(article) {
-    console.log(article.title);
-  });
-  // Each article has the following properties:
-  // 
-  //   * "title"     - The article title (String).
-  //   * "author"    - The author's name (String).
-  //   * "link"      - The original article link (String).
-  //   * "content"   - The HTML content of the article (String).
-  //   * "published" - The date that the article was published (Date).
-  //   * "feed"      - {name, source, link}
-  // 
-});
+var request = require("request")
+
+var url = "https://api.flickr.com/services/rest/?method=flickr.photos.search"+
+"&api_key=0cd074c5f7746588e9d018df9ecafb77"+
+"&user_id=55336091%40N08&per_page=50"+
+"&format=json&auth_token=72157663627636931-ae46ba6fea9b2a1e"+
+"&api_sig=fd66e4e1250072b3be95f5220f589921"
+
+request({
+    url: url,
+    json: true
+}, function (error, response, body) {
+
+    if (!error && response.statusCode === 200) {
+        console.log(body) // Print the json response
+    }
+})
