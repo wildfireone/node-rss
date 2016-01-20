b5e465df531f66a0dafb4e3a51e55d5abaf8916c
wildfireone <wildfireone@gmail.com>
2016-01-20 23:17:12 +0000

    Update process.js

--- process.js
+++ process.js
@@ -14,6 +14,6 @@ request({
 }, function (error, response, body) {
 
     if (!error && response.statusCode === 200) {
-        console.log(body) // Print the json response
+        console.log(body.photos) // Print the json response
     }
 })
