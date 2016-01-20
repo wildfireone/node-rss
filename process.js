b6d5120d41430e377534775295724864634e4371
wildfireone <wildfireone@gmail.com>
2016-01-20 23:41:55 +0000

    Update process.js

--- process.js
+++ process.js
@@ -4,7 +4,7 @@ var request = require("request")
 var url = "https://api.flickr.com/services/rest/?method=flickr.photos.search"+
 "&api_key=0cd074c5f7746588e9d018df9ecafb77"+
 "&user_id=55336091%40N08"+
-"&per_page=100"+
+"&per_page=50"+
 "&format=json"+
 "&nojsoncallback=1"+
 "&auth_token=72157663627636931-ae46ba6fea9b2a1e"+
